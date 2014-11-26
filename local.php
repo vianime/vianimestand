b716eb237648302616ac36ddd6aef4294e2fcfa5
Anderson <contacto.animedubes@gmail.com>
2014-11-25 20:04:59 -0500

    Update local.php

--- local.php
+++ local.php
@@ -7,24 +7,24 @@
  */
 
 // Sitio
-$slogan					= 'Nuestro eslogan es el anime';
-$titulopri				= 'VIAnime';
-$descripcion			= 'Descripcion del sitio';
-$keywords				= 'Palabras clave';
-$disqusname				= ''; // nombre de usuario de comentarios creados en disqus ejemplo: vianime
+$slogan               = 'Nuestro eslogan es el anime';
+$titulopri            = 'VIAnime';
+$descripcion          = 'Descripcion del sitio';
+$keywords             = 'Palabras clave';
+$disqusname           = ''; // nombre de usuario de comentarios creados en disqus ejemplo: vianime
 
-$buscar					= 'anime';
-$idioma					= 'español';
-$facebook_username		= '';
-$twitter_username		= '';
-$googleplus_username	= '';
-$terminos				= '#';
+$buscar               = 'anime';
+$idioma               = 'español';
+$facebook_username    = '';
+$twitter_username     = '';
+$googleplus_username  = '';
+$terminos             = '#';
 
 // Extensiones
-$extension				= true; // Configura la URL
-$imagen_remota			= true; 
-$show_hentai			= true; // Mostrar categoría hentai
-$show_shota				= true; // Mostrar categoría shota
-$show_loli				= true; // Mostrar categoría loli
-$Show_jPlayer			= false; // Reproductor HTML5 servidor propio
+$extension            = true; // Configura la URL
+$imagen_remota        = true; 
+$show_hentai          = true; // Mostrar categoría hentai
+$show_shota           = true; // Mostrar categoría shota
+$show_loli            = true; // Mostrar categoría loli
+$Show_jPlayer         = false; // Reproductor HTML5 servidor propio
 ?>
