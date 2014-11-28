b4c02dd957cd9ae4d22fec15790538a899ab8e6e
Anderson <contacto.animedubes@gmail.com>
2014-11-28 08:48:27 -0500

    Update index.php

--- index.php
+++ index.php
@@ -9,37 +9,8 @@
  * 
  * NOTA: La aplicación está basada en gran parte en el clon de AnimeID 2011.
  */
-require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'class/class.php';
-require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'carga.php';
 
-// Sesión
-if (isset($_SESSION['user_zero'])) {
-	$us = new Usersin();
-	$user = $us->ui($_SESSION['user_zero']);
+// Verificación de versión de PHP. Debe ser igual o mayor a PHP 5.5
+if (version_compare(phpversion(), '5.5.1', '<')) {
 }
-
-// Funsiones
-$obj = new Work();
-$barrauno = $obj->barra_uno();
-$obj = new Work();
-$capitulocom1 = $obj->lista_capitulos1();
-$obj = new Work();
-$ult15A = $obj->lista_animes15();
-$obj = new Work();
-$ult10O = $obj->lista_ovas10();
-$obj = new Work();
-$ult10P=$obj->lista_peliculas10();
-$obj = new Work();
-$ultimos = $obj->ultimos_agregados();
-$obj = new Work();
-$emision=$obj->lista_emision();
-$obj = new Work();
-$completa = $obj->lista_completaAOP();
-
-// Página
-$titulo = $slogan;
-$bodyclass = '';
-require_once HEADER;
-require_once dirname(__FILE__) . DS . 'home.php';
-require_once FOOTER;
 ?>
