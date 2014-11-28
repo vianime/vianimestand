cbb987343b0e76189718f5297e46b0450054d62a
Anderson <contacto.animedubes@gmail.com>
2014-11-28 08:45:13 -0500

    Update header.php

--- header.php
+++ header.php
@@ -1,3 +1,13 @@
 <?php
-
+/**
+ * VIAnime Stand 2014.
+ * Archivo de cabecera que caragará el estilo de la aplicación.
+ */
 ?>
+<!doctype html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
