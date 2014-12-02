73f1eae5a1a5c062727a3cc0bb5924d483c171ba
Anderson EspTeam <contacto.animedubes@gmail.com>
2014-12-02 17:25:24 -0500

    Reemplazando

--- vianime/index.php
+++ vianime/index.php
@@ -1,25 +1,9 @@
 <?php
 /**
- * Cojap v1.0.1
- * Desarrollado por Jose Buelvas Santos (ihojose).
- * Desarrollado en Suba, Bogotá D.C., Colombia.
- * Desarrollado el jueves 27 de noviembre de 2014.
+ * VIAnime - Si la aplicación está ejecutandose, entonces cargará
+ * este archivo de carga del tema principal (carpeta /temas/)
+ * Con el contenido indexado de datos.
  * 
- * Licencia del propietario.
+ * Versión 1.0.1stand del archivo
+ * creado el 2 de diciembre de 2014.
  */
- 
-// Verificación de versión de PHP. (PHP.5.5)
-if (version_compare(phpversion(), '5.5.1', '<')) {
-	printf('<strong>Cojap:</strong> Nesesitas la versión 5.5.1 de <a href="http://php.net">PHP</a> o superior para usar esta aplicación. Tu estás usando la versión <strong>%s</strong>', phpversion());
-	exit(1);
-}
-
-// Variables constantes definidas.
-define('_COJAP_BASE_', dirname($_SERVER['SCRIPT_NAME']));
-define('_COJAP_ARCHIVO_', $_SERVER['SCRIPT_NAME']);
-define('_COJAP_REL_', 'cojap');
-define('_COJAP_OBJETIVO_', 'index.php');
-define('_DS_', DIRECTORY_SEPARATOR);
-
-// Carga Principal
-require_once dirname(__FILE__) . _DS_ . _COJAP_REL_ . _DS_ . _COJAP_OBJETIVO_;
