3c791edc6b22060da0475c72fbb4e5a2c99ebc58
Anderson EspTeam <contacto.animedubes@gmail.com>
2014-12-06 21:32:06 -0500

    replanteando

--- vianime/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * VIAnime - Si la aplicación está ejecutandose, entonces cargará
- * este archivo de carga del tema principal (carpeta /temas/)
- * Con el contenido indexado de datos.
- * 
- * Versión 1.0.1stand del archivo
- * creado el 2 de diciembre de 2014.
- */
